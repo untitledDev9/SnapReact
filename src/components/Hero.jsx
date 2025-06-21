96923e9e0e84e8fc7914b77d821ac16f65b6cad2
untitled <ahmedsuleiman940@gmail.com>
2025-06-21 15:56:21 +0100

    ....

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,12 @@ import HeroImg from "../../public/image-hero-desktop.png"
 const Hero = () => {
     return (
         <div className=' flex justify-between items-center w-[80%] bg-amber-400 mx-auto '>
-            <div className=' w-[70%] bg-green-400'>
-                <h1 className=' text-[10rem] font-[600] w-[97%] leading-32'>Make
+            <div className=' flex flex-col items-start gap-10 w-[70%] bg-green-400'>
+                <h1 className=' text-[8rem] font-[600] leading-18'>Make <br />
                     remote work</h1>
-                <p>
+                <p className=' text-3xl text-[#6d6d6d]'>
                     Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
-                <button>Learn more</button>
+                <button className=' bg-black text-white rounded-xl py-4 px-7'>Learn more</button>
             </div>
             <div className=' w-1/2'>
                 <img src={HeroImg} alt=""  className=' w-[50%]'/>
