dada02d301362135b24cb85cd80a67e9acc07931
untitled <ahmedsuleiman940@gmail.com>
2025-06-21 20:22:12 +0100

    ......

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,24 +3,41 @@ import HeroImg from "../../public/image-hero-desktop.png"
 import HeroImgMobile from "../../public/image-hero-mobile.png"
 
 const Hero = () => {
-    return (
-        <div className=' bg-amber-200 sm:bg-green-400 md:bg-red-600 lg:bg-blue-600 xl:bg-purple-700 flex flex-col items-center mt-5 gap-6'>
-             <div className=' w-[80%]'>
-                <img src={HeroImgMobile} alt=""  className=' '/>
-            </div>
-            <div className=' flex flex-col justify-center text-center px-5 items-start gap-4 '>
-                <h1 className=' text-[3.5rem] font-[600] leading-18'>Make 
-                    remote work</h1>
-                <p className=' text-2xl text-[#6d6d6d] leading-7 '>
-                    Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
-                <button className=' bg-black mt-8 text-white rounded-xl py-4 px-7 mx-auto'>Learn more</button>
-            </div>
-            <div className=' hidden'>
-                <img src={HeroImg} alt=""  className=' '/>
-            </div>
-           
-        </div>
-    )
+  return (
+    <div className=' flex flex-col items-center mt-5 gap-6 
+    
+     md:flex-row md:w-[80%] mx-auto
+    '>
+      <div className=' w-[80%] md:hidden'>
+        <img src={HeroImgMobile} alt="" className=' ' />
+      </div>
+      <div className=' flex flex-col justify-center items-center text-center px-5 gap-4  
+      
+      
+md:w-[70%] md:items-start md:text-left
+      
+      
+     lg:w-[80%] lg:gap-10'>
+        <h1 className=' text-[3.5rem] font-[600] leading-18
+        
+       lg:w-[72%] lg:text-[6rem] lg:leading-17 '>Make
+          remote work</h1>
+        <p className=' text-2xl text-[#6d6d6d] leading-7 
+        
+       lg:text-2xl lg:w-[70%]'>
+          Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+        <button className=' bg-black mt-8 text-white rounded-xl py-4 px-7 mx-auto
+        
+       md:mx-0
+       
+       lg:mt-1'>Learn more</button>
+      </div>
+      <div className=' hidden md:flex w-[500px]'>
+        <img src={HeroImg} alt="" className=' ' />
+      </div>
+
+    </div>
+  )
 }
 
 export default Hero
